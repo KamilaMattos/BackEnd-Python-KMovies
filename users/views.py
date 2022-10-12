77ee0dab0116949cee9d241f1f377a055dbd3ea6
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2022-10-12 13:45:23 -0300

    FEAT: first task done

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView, Request, Response, status
+from .serializers import UserSerializer
+
+
+class UserRegisterView(APIView):
+    def post(self, req: Request) -> Response:
+        serializer = UserSerializer(data=req.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
